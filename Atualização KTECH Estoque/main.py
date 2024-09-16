f8f1c6ea5a3926cf578479cfdc0451218e29378e
willxprinxp <willianwrtwo@gmail.com>
2024-09-15 21:45:54 -0300

    Adicionando o botão na main
    O último compromisso de hoje. Ver se eu conseguia adicionar tranquilamente algo na Screen.

--- Atualização KTECH Estoque/main.py
+++ Atualização KTECH Estoque/main.py
@@ -1,10 +1,10 @@
 from lib_front import*
 
-log_screen = ConfigScreen('Login - KTECH Estoque', 'lightblue', 100, 300)
+log_screen = ConfigScreen('Login - KTECH Estoque', 'lightblue', 700, 500)
 log_screen.create_window()
 
 front_log = Screen(log_screen)
 title_log = front_log.create_label('Login Ktech Estoque', 'white')
-# log_screen.to_add(title_log, )
+log_screen.to_add(title_log, log_screen.screen_width, 20) 
 
 log_screen.init_window()
