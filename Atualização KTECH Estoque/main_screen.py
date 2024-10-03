bb34de0ebcbcc065fc7e75ea0a24c1c55fe21b35
willxprinxp <willianwrtwo@gmail.com>
2024-10-02 21:45:34 -0300

    Update main_screen.py
    Adicionando Objetos a main

--- Atualização KTECH Estoque/main_screen.py
+++ Atualização KTECH Estoque/main_screen.py
@@ -13,6 +13,13 @@ title_main = front_main.create_label('Ktech Estoque', main_screen.bg, 'lightblue
 main_screen.to_add(title_main, width=main_screen.screen_width, height=25)
 
 
+sidebar = front_main.create_frame(bg='lightblue')
+main_screen.to_add(sidebar, height=main_screen.screen_height, width=35)
+
+icon_user = front_main.to_icon(r'images\Usuário.png', (50,50))
+user_button = front_main.create_button(main_screen.sidebar, x=15, y=300)
+
+
 bloco_prod = front_main.create_button('Pesquisa de Blocos', bg='lightblue', fg='white')
 main_screen.to_add(bloco_prod, x=100, y=100)
 
