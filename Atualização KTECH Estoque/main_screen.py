b7f4f67119006f604e5d7dcfa2a1121d9374edba
willxprinxp <willianwrtwo@gmail.com>
2024-10-03 22:38:42 -0300

    Update main_screen.py
    Adicionando o botão de Usuário na main

--- Atualização KTECH Estoque/main_screen.py
+++ Atualização KTECH Estoque/main_screen.py
@@ -13,14 +13,15 @@ title_main = front_main.create_label('Ktech Estoque', main_screen.bg, 'lightblue
 main_screen.to_add(title_main, width=main_screen.screen_width, height=25)
 
 
-sidebar = front_main.create_frame(bg='lightblue')
-main_screen.to_add(sidebar, height=main_screen.screen_height, width=35)
+sidebar_main = front_main.create_frame(bg='lightblue')
+main_screen.to_add(sidebar_main, height=main_screen.screen_height, width=35)
 
-icon_user = front_main.to_icon(r'images\Usuário.png', (50,50))
-user_button = front_main.create_button(main_screen.sidebar, x=15, y=300)
+icon_user = front_main.to_icon(r'.venv\images\Usuario.png', (25,25))
+user_button = front_main.create_button('', image=icon_user, bg='lightblue', relief='flat', activebackground='lightblue', command=main_screen.button_clicked('Ícone Clicado'))
+main_screen.to_add(user_button, x=2, y=main_screen.screen_height-100)
 
 
-bloco_prod = front_main.create_button('Pesquisa de Blocos', bg='lightblue', fg='white')
+bloco_prod = front_main.create_button('Pesquisa de Blocos', bg='lightblue', fg='white', command=front_main.button_clicked('Botão Clicado'))
 main_screen.to_add(bloco_prod, x=100, y=100)
 
 main_screen.init_window()
